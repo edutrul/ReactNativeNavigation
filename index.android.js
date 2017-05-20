edd1ba9b7fd66fcb67c187021991437a1d667f03
Eduardo Telaya <luis.eduardo.telaya@gmail.com>
2017-05-19 22:28:10 -0500

    WIP navigation

--- index.android.js
+++ index.android.js
@@ -1,21 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {
   AppRegistry,
   Text,
+  View,
+  Button,
+  StyleSheet,
+  Alert
 } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 
-class HomeScreen extends React.Component {
+
+class ChatScreen extends Component {
+  static navigationOptions = {
+    title: 'Chat with Lucy',
+  };
+  render() {
+    return (
+      <View>
+        <Text>Chat with Lucy</Text>
+      </View>
+    );
+  }
+}
+
+class HomeScreen extends Component {
   static navigationOptions = {
     title: 'Welcome',
   };
   render() {
-    return <Text>Hello, Navigation!</Text>;
+    const { navigate } = this.props.navigation;
+    return (
+      <View>
+        <Text>Hello, Chat App!</Text>
+        <Button
+          onPress = {() => Alert.alert('Alert Title', 'yayay')}
+          title = 'Open Other screen'
+          />
+      </View>
+    );
   }
 }
 
-const SimpleApp = StackNavigator({
+const NavigationProject = StackNavigator({
   Home: { screen: HomeScreen },
+  Chat: { screen: ChatScreen },
 });
 
-AppRegistry.registerComponent('SimpleApp', () => SimpleApp);
+AppRegistry.registerComponent('NavigationProject', () => NavigationProject);
