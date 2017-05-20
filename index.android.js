3b53b239a9768934d28d1e62c2fc28194fade226
Eduardo Telaya <luis.eduardo.telaya@gmail.com>
2017-05-19 21:40:29 -0500

    Add initial navigation WIP

--- index.android.js
+++ index.android.js
@@ -1,56 +1,21 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
-import React, { Component } from 'react';
+import React from 'react';
 import {
   AppRegistry,
-  StyleSheet,
   Text,
-  View,
-  Image
 } from 'react-native';
+import { StackNavigator } from 'react-navigation';
 
-import HelloWorld from './components/HelloWorld';
-
-export default class NavigationProject extends Component {
+class HomeScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Welcome',
+  };
   render() {
-    let images = [
-      'http://teamoalexandra.tk/img/01.jpg',
-      'http://teamoalexandra.tk/img/02.jpg',
-      'http://teamoalexandra.tk/img/03.jpg',
-      'http://teamoalexandra.tk/img/04.jpg'
-    ];
-    let helloWorld = new HelloWorld('Eduardo!');
-
-    return (
-      <View style={styles.container}>
-        <Text>{helloWorld.getMessage()}</Text>
-        {images.map(function(image) { return <Image source={{uri: image}} style={{width: 193, height: 110}}/>; } }
-      </View>
-    );
+    return <Text>Hello, Navigation!</Text>;
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+const SimpleApp = StackNavigator({
+  Home: { screen: HomeScreen },
 });
 
-AppRegistry.registerComponent('NavigationProject', () => NavigationProject);
+AppRegistry.registerComponent('SimpleApp', () => SimpleApp);
