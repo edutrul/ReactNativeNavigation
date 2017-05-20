c5e9d1f41b314f1a2c28b4911115a67bcdda9796
Eduardo Telaya <luis.eduardo.telaya@gmail.com>
2017-05-20 01:03:06 -0500

    Add real navigation

--- index.android.js
+++ index.android.js
@@ -15,13 +15,50 @@ class ChatScreen extends Component {
     title: 'Chat with Lucy',
   };
   render() {
+    const { navigate } = this.props.navigation;
     return (
       <View>
         <Text>Chat with Lucy</Text>
+        <Button
+          onPress={() => navigate('Third')}
+          title='Open third screen'
+        />
       </View>
     );
   }
 }
+
+class ThirdScreen extends Component {
+  static navigationOptions = {
+    title: 'Third screen oh yeah',
+  };
+  render() {
+    const { navigate } = this.props.navigation;
+    return (
+      <View>
+        <Text>This is a third screen</Text>
+        <Button
+          onPress={() => navigate('Fourth')}
+          title='Open fourth screen'
+        />
+      </View>
+    );
+  }
+}
+
+class FourthScreen extends Component {
+  static navigationOptions = {
+    title: 'Fourth screen oh yeah',
+  };
+  render() {
+    return (
+      <View>
+        <Text>This is a Fourth screen</Text>
+      </View>
+    );
+  }
+}
+
 
 class HomeScreen extends Component {
   static navigationOptions = {
@@ -33,8 +70,16 @@ class HomeScreen extends Component {
       <View>
         <Text>Hello, Chat App!</Text>
         <Button
-          onPress = {() => Alert.alert('Alert Title', 'yayay')}
-          title = 'Open Other screen'
+          onPress={() => navigate('Chat')}
+          title = 'Open Other Screen'
+          />
+        <Button
+          onPress={() => navigate('Third')}
+          title = 'Open fourth Screen'
+          />
+        <Button
+          onPress={() => navigate('Fourth')}
+          title = 'Open fourth Screen'
           />
       </View>
     );
@@ -44,6 +89,8 @@ class HomeScreen extends Component {
 const NavigationProject = StackNavigator({
   Home: { screen: HomeScreen },
   Chat: { screen: ChatScreen },
+  Third: { screen: ThirdScreen },
+  Fourth: { screen: FourthScreen },
 });
 
 AppRegistry.registerComponent('NavigationProject', () => NavigationProject);
